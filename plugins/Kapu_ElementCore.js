3eb8472d7200200d8c65ed3c2d992b2380cc939b
kapuusagi <usagizinrui@yahoo.co.jp>
2020-09-16 17:23:10 +0900

    属性に関する処理で、オリジナル実装のAPIだけだとうまく処理できないのでCoreを介して拡張するように変更した。

---
+++ plugins/Kapu_ElementCore.js
@@ -0,0 +1,139 @@
+/*:ja
+ * @target MZ 
+ * @plugindesc 属性処理についてのいくつかの拡張を行うプラグイン
+ * @author kapuusagi
+ * @url https://github.com/kapuusagi/MZPlugins/tree/master/plugins
+ * @base Kapu_Utility
+ * 
+ * @help 
+ * 属性処理について、以下の拡張を行います。
+ * ・スキルに複数属性を持たせる。(elementIdsノートタグ参照。)
+ * ・異なるプラグインで競合しにくくするメソッド変更。（プラグイン開発者向けを参照）
+ * 
+ * ■ 注意
+ * 以下のメソッドがオーバーライドされるため、競合に注意が必要です。
+ * Game_Action.prototype.elementRate
+ * Game_Action.prototype.elementsMaxRate
+ * 
+ * ■ プラグイン開発者向け
+ * $dataSkill, $Items, $dataWeaponsのdamageにelementIdsフィールドを追加します。
+ * Game_Action.elementRateをオーバーライドします。
+ * Game_Action.elementsMaxRateをオーバーライドします。
+ * Game_Action.getApplyElementRateを追加します。
+ * 複数属性指定時のレート配列を受け取り、適用するレートを返します。
+ * 乗算結果を返したい場合にはこのメソッドをオーバーライドすれば済みます。
+ * Game_Action.singleElementRateを追加します。
+ * 使用者から対象への1つの属性倍率を操作する場合、このメソッドをフックすれば済みます。
+ * 
+ * 複数属性適用時の演算処理は、ベーシックシステムにあわせて最大の効果量を返すようにしてありますが、
+ * 乗算結果にしたい場合にはelementsMaxRateをオーバーライドします。
+ * 
+ * ============================================
+ * プラグインコマンド
+ * ============================================
+ * プラグインコマンドはありません。
+ * 
+ * ============================================
+ * ノートタグ
+ * ============================================
+ * 武器/スキル
+ *     <elementIds:id#>
+ *     <elementIds:id#,id#,id#...>
+ *         複数属性を適用します。
+ *
+ * ============================================
+ * 変更履歴
+ * ============================================
+ * Version.0.1.0 新規作成。
+ */
+(() => {
+    const pluginName = 'Kapu_ElementCore';
+    const parameters = PluginManager.parameters(pluginName);
+
+
+    //------------------------------------------------------------------------------
+    // DataManager
+    /**
+     * elementAbsorb ノートタグを処理する。
+     * 
+     * @param {Object} obj データオブジェクト
+     */
+    const _processElementIdsNotetag = function(obj) {
+        if (!"damage" in obj) {
+            return;
+        }
+        obj.damage.elementIds = [];
+        if (obj.damage.elementId) {
+            obj.damage.elementIds.push(obj.damage.elementId);
+        }
+
+        if (!obj.meta.elementIds) {
+            return;
+        }
+        const valueStr = obj.meta.elementIds;
+        const tokens = valueStr.split(',');
+        for (token of tokens) {
+            const elementId = Number(token);
+            if (elementId) {
+                obj.damage.elementIds.push(elementId);
+            }
+        }
+        if ((obj.damage.elementId < 0) && (obj.damage.elementIds.length > 0)) {
+            obj.damage.elementId = Math.min(...obj.damage.elementIds);
+        }
+    };
+    DataManager.addNotetagParserWeapons(_processElementIdsNotetag);
+    DataManager.addNotetagParserItems(_processElementIdsNotetag);
+
+    //------------------------------------------------------------------------------
+    // Game_Action
+    /**
+     * 属性による効果倍率を得る。
+     * 
+     * @param {Game_BattlerBase} target ターゲット
+     * @return {Number} 効果倍率
+     * !!!overwrite!!!
+     */
+    Game_Action.prototype.calcElementRate = function(target) {
+        const elements = (this.item().damge.elementIds.length > 0) 
+            ? this.item().damage.elementIds : this.subject().attackElements();
+        return this.elementsMaxRate(target, elements);
+    };
+
+    /**
+     * 複数属性による効果倍率を得る。
+     * 既定の実装では単純に最大値のレートを返す。
+     * 
+     * @param {Game_BattlerBase} target ターゲット
+     * @return {Number} 効果倍率
+     * !!!overwrite!!!
+     */
+    Game_Action.prototype.elementsMaxRate = function(target, elements) {
+        const elementRates = [];
+        for (elementId of elements) {
+            elementRates[elementId] = this.singleElementRate(target, elementId);
+        }
+        return this.getApplyElementRate(elements, elementRates);
+    };
+
+    /**
+     * 複数属性指定時のレート配列を受け取り、適用するレートを返す。
+     * 
+     * @param {Array<Number>} elements 属性ID配列。
+     * @param {Array<Number>} elementRates IDに対応した倍率。elementRates[elementId]で値がとれる。
+     */
+    Game_Action.prototype.getApplyElementRate = function(elements, elementRates) {
+        return Math.max(...elementRates);
+    };
+
+    /**
+     * 単一の属性効果を得る。
+     * 
+     * @param {Game_BattlerBase} target 
+     * @param {Number} elementId 属性ID
+     * @return {Number} 属性倍率
+     */
+    Game_Action.prototype.singleElementRate = function(target, elementId) {
+        return target.elementRate(elementId);
+    };
+})();
---
+++ plugins/Kapu_Trait_AttackElementRate.js
@@ -0,0 +1,128 @@
+/*:ja
+ * @target MZ 
+ * @plugindesc 属性攻撃時のダメージ量を割合で増加させる特性を追加するプラグイン。
+ * @author kapuusagi
+ * @url https://github.com/kapuusagi/MZPlugins/tree/master/plugins
+ * @base Kapu_ElementCore
+ * 
+ * @param TraitCode
+ * @text 特性コード
+ * @desc 特性として割り当てるID番号。(65以上で他のプラグインとぶつからないやつ)
+ * @default 101
+ * @type number
+ * @max 999
+ * @min 65
+ * 
+ * @help 
+ * 属性攻撃時のダメージ量を割合で増加させる特性を追加する。
+ * 
+ * ■ プラグイン開発者向け
+ * 新規Trait を使用し、
+ * Game_BattlerBase.TRAIT_ELEMENT_ATTACK_RATE を追加します。
+ * 値はプラグインパラメータで指定したものになります。
+ * 例えば、一時的に火属性のダメージだけ増加させたい、という場合に使用できます。
+ * 
+ * ============================================
+ * プラグインコマンド
+ * ============================================
+ * プラグインコマンドはありません。
+ * 
+ * ============================================
+ * ノートタグ
+ * ============================================
+ * アクター/クラス/ステート/武器/防具/エネミー
+ *     <elementAttackRate:id#:data,...>
+ *        吸収特性設定。dataの記述書式は次の通り。
+ *        id#:rate# もしくは id#:rate%
+ *        id#:属性ID
+ *        rate#:加算ダメージレート(0～, 1.0で変化なし)
+ *        rate%:加算ダメージレート(0～, 1.0で変化なし)
+ *        複数吸収設定はカンマ区切りにします。
+ * 
+ *     例：
+ *        <elementAttackRate:1:20%,3:100%>
+ *          属性ID 1 の攻撃を受けたときは20%吸収。 属性ID 3 を受けたときは100%吸収。
+ * ============================================
+ * 変更履歴
+ * ============================================
+ * Version.0.1.0 思いついたので追加した。
+ */
+(() => {
+    const pluginName = "Kapu_Trait_AttackElementRate";
+    const parameters = PluginManager.parameters(pluginName);
+
+    Game_BattlerBase.TRAIT_ELEMENT_ATTACK_RATE = Number(parameters['TraitCode']) || 0;
+
+    //------------------------------------------------------------------------------
+    // DataManager
+
+    /**
+     * 割合パラメータを計算する。
+     * 
+     * @param {String} valueStr 文字列
+     */
+    const _parseRate = function(valueStr) {
+        if (valueStr.slice(-1) === "%") {
+            return Number(valueStr.slice(0, valueStr.length - 1)) / 100.0;
+        } else {
+            return Number(valueStr);
+        }
+    };
+
+    /**
+     * elementAbsorb ノートタグを処理する。
+     * 
+     * @param {Object} obj データオブジェクト
+     */
+    const _processElementAttackRateNoteTag = function(obj) {
+        if (!obj.meta.elementAttackRate) {
+            return;
+        }
+        const valueStr = obj.meta.elementAttackRate;
+        const entries = valueStr.split(',');
+        for (entry of entries) {
+            const tokens = entry.split(':');
+            if (tokens.length >= 2) {
+                const elementId = Number(tokens[0]);
+                const rate = _parseRate(tokens[1].trim());
+                if (elementId && rate) {
+                    obj.traits.push({ 
+                        code:Game_BattlerBase.TRAIT_ELEMENT_ATTACK_RATE, 
+                        dataId:elementId, 
+                        value:rate
+                    });
+                }
+            }
+        }
+    };
+
+    DataManager.addNotetagParserActors(_processElementAttackRateNoteTag);
+    DataManager.addNotetagParserClasses(_processElementAttackRateNoteTag);
+    DataManager.addNotetagParserWeapons(_processElementAttackRateNoteTag);
+    DataManager.addNotetagParserArmors(_processElementAttackRateNoteTag);
+    DataManager.addNotetagParserStates(_processElementAttackRateNoteTag);
+    DataManager.addNotetagParserEnemies(_processElementAttackRateNoteTag);
+
+    //------------------------------------------------------------------------------
+    // Game_BattlerBase
+    Game_BattlerBase.prototype.elementAttackrate = function(elementId) {
+        return this.traitsPi(Game_BattlerBase.TRAIT_ELEMENT_ATTACK_RATE, elementId);
+    };
+
+    //------------------------------------------------------------------------------
+    // Game_Action
+    const _Game_Action_singleElementRate = Game_Action.prototype.singleElementRate;
+
+    /**
+     * 対象に対する単一の属性効果を得る。
+     * 
+     * @param {Game_BattlerBase} target 
+     * @param {Number} elementId 属性ID
+     * @return {Number} 属性倍率
+     */
+    Game_Action.prototype.singleElementRate = function(target, elementId) {
+        let rate = _Game_Action_singleElementRate.call(this, ...targets);
+        rate *= this.subject().elementAttackRate(elementId);
+        return rate;
+    };
+})();
