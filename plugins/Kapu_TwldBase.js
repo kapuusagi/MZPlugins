ff2fa945c04b7514d57cd59d748b10c5f7e55def
kapuusagi <usagizinrui@yahoo.co.jp>
2020-09-30 14:27:21 +0900

    TwldCoreをTwldBaseに変更した。

--- plugins/Kapu_TwldBase.js
+++ plugins/Kapu_TwldBase.js
@@ -1,15 +1,15 @@
 /*:ja
  * @target MZ 
- * @plugindesc TWLDコアシステムプラグイン
+ * @plugindesc TWLDベースシステムプラグイン
  * @author kapuusagi
  * @url https://github.com/kapuusagi/MZPlugins/tree/master/plugins
- * 
  * 
  * @help 
  * TWLD向けに、ベーシックシステムの計算式等を変更するプラグイン。
  * 
  * 1.アクション適用判定の変更
- *    
+ *    クリティカル発生時は確実に命中とする。
+ *    miss判定はせず、evadのみ結果を格納する。但し命中したかどうかはHITとEVAを考慮する。
  * 
  * ■ 使用時の注意
  * 
@@ -31,7 +31,7 @@
  * Version.0.1.0 動作未確認。
  */
 (() => {
-    const pluginName = "Kapu_TwldCore";
+    const pluginName = "Kapu_TwldBase";
     const parameters = PluginManager.parameters(pluginName);
 
     // PluginManager.registerCommand(pluginName, "TODO:コマンド。@commsndで指定したやつ", args => {
@@ -48,11 +48,10 @@
      * !!!overwrite!!! Game_Action.apply
      */
     Game_Action.prototype.apply = function(target) {
-        var result = target.result(); // Game_ActionResultオブジェクト
-        this.subject().clearResult(); // 使用者側の結果をクリア？？
+        const result = target.result();
+        this.subject().clearResult();
         result.clear(); // 対象の結果をクリア
-        result.used = this.testApply(target); // 対象に適用可能かを調べる。(戦闘不能の相手にダメージ与えるとかないよね。)
-
+        result.used = this.testApply(target); // 対象に適用可能かを調べる。
         // クリティカル判定。クリティカルしたら、問答無用で当てる。
         if (this.item().damage.critical) {
             result.critical = (Math.random() < this.itemCri(target));
@@ -78,7 +77,7 @@
             if (this.item().damage.type > 0) {
                 var value = this.makeDamageValue(target, result.critical);
                 this.executeDamage(target, value);
-                this._damageValue = value;
+                // this._damageValue = value;
             }
             this.item().effects.forEach(function(effect) {
                 this.applyItemEffect(target, effect);
@@ -86,6 +85,40 @@
             this.applyItemUserEffect(target);
         }
     };
+
+    /**
+     * 当たるかどうかを判定する。
+     * 
+     * @param {Game_Battler} target 対象
+     */
+    Game_Action.prototype.testHit = function(target) {
+        var subject = this.subject(); // 使用者
+        var hitRate = this.itemHit(target); 
+        if (this.isPhysical()) {
+            var pev = target.eva // 回避率は純粋に効いてくる
+                - (hitRate - 1.0) // 命中率分の補正。1.0超えた分は命中しやすくなる。
+                + (target.luk - subject.luk) * 0.01; // LUKによるあたり判定(LUK高いやつは当てやすい)
+            if (this.isLongRange()) {
+                // 長距離の場合には相手の素早さと使用者の器用さを比較する。
+                pev += this.getRelativeDiff(target.agi, subject.dex);
+            } else {
+                // 接近戦の場合にはDEXとAGIの合計値を比較する。
+                pev += this.getRelativeDiff((target.dex + target.agi), (subject.dex + subject.agi));
+            }
+            return Math.random() >= pev;
+        } else if (this.isMagical()) {
+            var mev = target.mev // 魔法回避率は純粋に効いてくる。
+                - (hitRate - 1.0) // 命中率の1.0超えた分は相手の回避率を減らす方向に効く。
+                + this.getRelativeDiff(target.men, subject.men) / 2
+                + (target.luk - subject.luk) * 0.01; // LUKによるあたり判定(LUK高いやつは当てやすい)
+                return Math.random() >= mev;
+        } else {
+            return true; // その他は100%成功
+        }
+    };
+
+    //------------------------------------------------------------------------------
+    // Game_Actor
     /**
      * パッシブスキルを持っているかどうかを判定する。
      * @return パッシブスキルを持っている場合にはtrue, それ以外はfalse
