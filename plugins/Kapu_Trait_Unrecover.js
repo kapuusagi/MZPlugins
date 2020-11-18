a15cfc37c017b1279d87e00bba3497150a22ea57
usagizinrui <usagizinrui@yahoo.co.jp>
2020-11-18 22:09:17 +0900

    依存モジュール名が間違っている不具合を修正した。

--- plugins/Kapu_Trait_Unrecover.js
+++ plugins/Kapu_Trait_Unrecover.js
@@ -5,8 +5,8 @@
  * @url https://github.com/kapuusagi/MZPlugins/tree/master/plugins
  * @base Kapu_Utility
  * @orderAfter Kapu_Utility
- * @base Kapu_DamageCalculation
- * @orderAfter Kapu_DamageCalculation
+ * @base Kapu_Base_DamageCalculation
+ * @orderAfter Kapu_Base_DamageCalculation
  * 
  * @param unrecoveryTraitCode
  * @text フラグID
