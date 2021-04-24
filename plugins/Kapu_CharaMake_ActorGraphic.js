38c162f67a4673f8c99b59173bcb0fe6856fc053
usagizinrui <usagizinrui@yahoo.co.jp>
2021-04-24 13:38:46 +0900

    [FixedBug]拡張用にnoteフィールドを追加した。

--- plugins/Kapu_CharaMake_ActorGraphic.js
+++ plugins/Kapu_CharaMake_ActorGraphic.js
@@ -18,7 +18,11 @@
  * @type string
  * @default ビジュアルを変更します。
  * 
- * 
+ * @param textItemNameDefault
+ * @text デフォルト選択項目冥
+ * @description リストに無い場合に表示する項目名として使用されるテキスト
+ * @type string
+ * @default デフォルト
  * 
  * @param charaMakeItems
  * @text キャラクタメイキング選択項目
@@ -26,13 +30,23 @@
  * @type struct<GraphicEntry>[]
  * 
  * 
+ * 
+ * 
  * @help 
  * キャラメイクに単純なグラフィック(顔と歩行グラフィック、バトラー)選択を追加するプラグインです。
  * 部品を組み合わせて自由にキャラメイクをするものではないです。
  * 職業のノートタグにより、選択可否条件を付けることができます。
  * 
+ * 使用方法
+ * 1. プラグインパラメータのcharaMakeItemsを編集して設定項目エントリを作ります。
+ * 2. Kapu_ChraMakeプラグインのキャラメイクシーンを呼び出します。
+ * 
  * ■ 使用時の注意
  * 特になし。
+ * 
+ * ■ プラグイン開発者向け
+ * プラグインパラメータ charaMakeItems のnoteフィールドは item.meta フィールドに展開されます。
+ * 
  * 
  * ============================================
  * プラグインコマンド
@@ -42,11 +56,12 @@
  * ============================================
  * ノートタグ
  * ============================================
+ * ノートタグはありません。
  * 
  * ============================================
  * 変更履歴
  * ============================================
- * Version.0.1.0 動作未確認。
+ * Version.1.0.0 初版作成。
  */
 /*~struct~GraphicEntry:
  *
@@ -87,13 +102,19 @@
  * @text サイドビューバトラーファイル
  * @description サイドビューバトルグラフィックファイル名
  * @type file
- * @dir img/battlers/
+ * @dir img/sv_actors/
+ * 
+ * @param note
+ * @text ノートタグ
+ * @description ノートタグとして使用するテキスト
+ * @type string
+ * @default
  */
 
 /**
  * グラフィック選択アイテム
  */
-function Game_CharaMakeItem_Graphic() {
+function Game_CharaMakeItem_Visual() {
     this.initialize(...arguments);
 }
 
@@ -106,20 +127,43 @@ function Window_CharaMakeVisualSelection() {
 
 
 (() => {
-    const pluginName = "Kapu_CharaMake_Graphic";
+    const pluginName = "Kapu_CharaMake_ActorGraphic";
     const parameters = PluginManager.parameters(pluginName);
     const textItemNameGraphic = parameters["textItemNameGraphic"] || "Graphic";
     const textItemDescriptionGraphic = parameters["textItemDescriptionGraphic"] || "Select graphic.";
+    const textItemNameDefault = parameters["textItemNameDefault"] || "default";
     const graphicItems = [];
     try {
         const entries = JSON.parse(parameters["charaMakeItems"]).map(str => JSON.parse(str));
         for (entry of entries) {
+            // noteフィールドを展開する。
+            if (entry.node) {
+                DataManager.extractMetadata(entry);
+            } else {
+                entry.note = "";
+                entry.meta = {};
+            }
             graphicItems.push(entry);
         }
     }
     catch (e) {
         console.log(e);
     }
+
+    //------------------------------------------------------------------------------
+    // DataManager
+    const _DataManager_createCharaMakeItems = DataManager.createCharaMakeItems;
+    /**
+     * キャラクターメイキング項目を取得する。
+     * キャラメイク項目を拡張する場合、このメソッドをフックして値を配列に加えて返す。
+     * 
+     * @return {Array<Game_CharaMakeItem>} キャラクターメイキング項目
+     */
+    DataManager.createCharaMakeItems = function() {
+        const items = _DataManager_createCharaMakeItems.call(this);
+        items.push(new Game_CharaMakeItem_Visual());
+        return items;
+    };    
     //------------------------------------------------------------------------------
     // Window_CharaMakeVisualSelection
     Window_CharaMakeVisualSelection.prototype = Object.create(Window_CharaMakeItemSelection.prototype);
@@ -152,6 +196,23 @@ function Window_CharaMakeVisualSelection() {
     };
 
     /**
+     * アイテムリストを設定する。
+     * 
+     * @param {Array<object>} items アイテムリスト (objectにnameメンバを持つこと)
+     */
+    Window_CharaMakeVisualSelection.prototype.setItems = function(items) {
+        for (const item of items) {
+            if (item.faceName) {
+                ImageManager.loadFace(item.faceName);
+            }
+            if (item.characterName) {
+                ImageManager.loadCharacter(item.characterName);
+            }
+        }
+        Window_CharaMakeItemSelection.prototype.setItems.call(this, items);
+    };
+
+    /**
      * 項目を描画する。
      * 
      * @param {Number} index インデックス番号
@@ -159,6 +220,8 @@ function Window_CharaMakeVisualSelection() {
     Window_CharaMakeVisualSelection.prototype.drawItem = function(index) {
         const rect = this.itemRect(index);
         const item = this._items[index];
+
+        this.resetFontSettings();
 
         // DrawFace
         const faceX = rect.x + (rect.width - ImageManager.faceWidth) / 2;
@@ -173,12 +236,15 @@ function Window_CharaMakeVisualSelection() {
         // 名前背景
         const nameX = rect.x;
         const nameY = rect.y + rect.height - this.lineHeight();
+        this.changePaintOpacity(false);
         this.changeTextColor(ColorManager.itemBackColor1());
         this.changeOutlineColor(ColorManager.itemBackColor2());
         this.drawRect(nameX, nameY, rect.width, this.lineHeight());
         this.resetTextColor();
+        this.changePaintOpacity(true);
 
         // 名前
+        this.makeFontSmaller();
         this.drawText(item.name, nameX, nameY, rect.width);
     };
     /**
@@ -197,14 +263,14 @@ function Window_CharaMakeVisualSelection() {
         }
     };
     //------------------------------------------------------------------------------
-    // Game_CharaMakeItem_Graphic
-    Game_CharaMakeItem_Graphic.prototype = Object.create(Game_CharaMakeItem.prototype);
-    Game_CharaMakeItem_Graphic.prototype.constructor = Game_CharaMakeItem_Graphic;
+    // Game_CharaMakeItem_Visual
+    Game_CharaMakeItem_Visual.prototype = Object.create(Game_CharaMakeItem.prototype);
+    Game_CharaMakeItem_Visual.prototype.constructor = Game_CharaMakeItem_Visual;
 
     /**
-     * Game_CharaMakeItem_Graphicを初期化する。
+     * Game_CharaMakeItem_Visualを初期化する。
      */
-    Game_CharaMakeItem_Graphic.prototype.initialize = function() {
+    Game_CharaMakeItem_Visual.prototype.initialize = function() {
         Game_CharaMakeItem.prototype.initialize.call(this, ...arguments);
     };
 
@@ -213,7 +279,7 @@ function Window_CharaMakeVisualSelection() {
      * 
      * @return {string} キャラクターメイキングの項目名として使用される。
      */
-    Game_CharaMakeItem_Graphic.prototype.name = function() {
+    Game_CharaMakeItem_Visual.prototype.name = function() {
         return textItemNameGraphic;
     };
     /**
@@ -221,7 +287,7 @@ function Window_CharaMakeVisualSelection() {
      * 
      * @return {string} キャラクターメイキングの項目名として使用される。
      */
-    Game_CharaMakeItem_Graphic.prototype.description = function() {
+    Game_CharaMakeItem_Visual.prototype.description = function() {
         return textItemDescriptionGraphic;
     };
 
@@ -233,7 +299,7 @@ function Window_CharaMakeVisualSelection() {
      * @returns {Window_Selectable} ウィンドウ(Window_Selectableの派生クラス)
      */
     // eslint-disable-next-line no-unused-vars
-    Game_CharaMakeItem_Graphic.prototype.createSelectWindows = function(rect, helpWindow) {
+    Game_CharaMakeItem_Visual.prototype.createSelectWindows = function(rect, helpWindow) {
         const window = new Window_CharaMakeVisualSelection(rect);
         window.setItems(this.items())
         return {
@@ -252,7 +318,7 @@ function Window_CharaMakeVisualSelection() {
      * @param {Game_Actor} acotr アクター
      */
     // eslint-disable-next-line no-unused-vars
-    Game_CharaMakeItem_Graphic.prototype.setCurrent = function(windowEntry, actor) {
+    Game_CharaMakeItem_Visual.prototype.setCurrent = function(windowEntry, actor) {
         const selectWindow = windowEntry.selectWindow;
         const items = selectWindow.items();
         let index = 0;
@@ -272,12 +338,12 @@ function Window_CharaMakeVisualSelection() {
             selectWindow.select(index);
         } else {
             items.unshift({
-                name: actor.name() || "Custom",
+                name: actor.name() || textItemNameDefault,
                 characterName: actor.characterName(),
                 characterIndex: actor.characterIndex(),
                 faceName: actor.faceName(),
                 faceIndex: actor.faceIndex(),
-                batterName: actor.batterName()
+                battlerName: actor.battlerName()
             });
             selectWindow.setItems(items);
             selectWindow.select(0);
@@ -291,11 +357,11 @@ function Window_CharaMakeVisualSelection() {
      * @param {Game_Actor} acotr アクター
      */
     // eslint-disable-next-line no-unused-vars
-    Game_CharaMakeItem_Graphic.prototype.apply = function(windowEntry, actor) {
+    Game_CharaMakeItem_Visual.prototype.apply = function(windowEntry, actor) {
         const selectWindow = windowEntry.selectWindow;
         const item = selectWindow.item();
         actor.setCharacterImage(item.characterName, item.characterIndex);
-        acotr.setFaceImage(item.faceName, item.faceIndex);
+        actor.setFaceImage(item.faceName, item.faceIndex);
         actor.setBattlerImage(item.battlerName);
     };
 
@@ -304,7 +370,7 @@ function Window_CharaMakeVisualSelection() {
      * 
      * @return {Array<object>} アイテム一覧
      */
-    Game_CharaMakeItem_Graphic.prototype.items = function() {
+    Game_CharaMakeItem_Visual.prototype.items = function() {
         return graphicItems;
     };
 
