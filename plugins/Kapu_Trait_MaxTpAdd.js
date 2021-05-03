c88029158ec430100deaca807a110b75ec7dee1b
usagizinrui <usagizinrui@yahoo.co.jp>
2021-05-03 16:35:37 +0900

    [AddNewFeature]Kapu_Base_ParamNameに対応

--- plugins/Kapu_Trait_MaxTpAdd.js
+++ plugins/Kapu_Trait_MaxTpAdd.js
@@ -7,6 +7,7 @@
  * @orderAfter Kapu_Utility
  * @base Kapu_Base_Params
  * @orderAfter Kapu_Base_Params
+ * @orderAfter Kapu_Base_ParamName
  * 
  * @param traitCode
  * @text 最大TP加算特性コードID
@@ -15,6 +16,19 @@
  * @type number
  * @max 999
  * @min 10
+ * 
+ * @param textTraitMaxTpAddUp
+ * @text TP加算特性名
+ * @desc TP加算特性名(増加)
+ * @type string
+ * @default 最大TP上昇
+ * 
+ * @param textTraitMaxTpAddDown
+ * @text TP加算特性名
+ * @desc TP加算特性名(減少)
+ * @type string
+ * @default 最大TP減少
+ * 
  *  
  * @help 
  * 最大TP加算特性を追加するプラグイン。
@@ -43,18 +57,21 @@
  * ============================================
  * 変更履歴
  * ============================================
- * Version.0.1.0 作成した。
+ * Version.0.2.0 Kapu_Base_ParamNameに対応。
+ * Version.0.1.0 新規作成。
  */
 (() => {
     const pluginName = "Kapu_Trait_MaxTpAdd";
     const parameters = PluginManager.parameters(pluginName);
-
 
     Game_BattlerBase.TRAIT_MAXTP_ADD = Number(parameters["traitCode"]) || 0;
     if (!Game_BattlerBase.TRAIT_MAXTP_ADD) {
         console.error(pluginName + ":TRAIT_MAXTP_ADD is not valid.");
         return;
     }
+
+    const textTraitMaxTpAddUp = parameters["textTraitMaxTpAddUp"] || "";
+    const textTraitMaxTpAddDown = parameters["textTraitMaxTpAddDown"] || "";
 
     //------------------------------------------------------------------------------
     // DataManager
@@ -83,6 +100,26 @@
     DataManager.addNotetagParserArmors(_processNotetag);
     DataManager.addNotetagParserStates(_processNotetag);
     DataManager.addNotetagParserEnemies(_processNotetag);
+
+    //------------------------------------------------------------------------------
+    // TextManager
+    /**
+     * 最大TP加算特性名
+     * 
+     * @param {number} dataId データID
+     * @param {number} value 値
+     * @returns {string} 文字列
+     */
+    // eslint-disable-next-line no-unused-vars
+    TextManager.traitMaxTpAdd = function(dataId, value) {
+        return (value >= 0) ? textTraitMaxTpAddUp : textTraitMaxTpAddDown;
+    };
+    if (TextManager._traitConverters) {
+        TextManager._traitConverters[Game_BattlerBase.TRAIT_MAXTP_ADD] = {
+            name:TextManager.traitMaxTpAdd, value:TextManager.traitValueSum, str:TextManager.traitValueStrInt, baseValue:0
+        };
+    }
+    
     //------------------------------------------------------------------------------
     // Game_BattlerBase
     const _Game_BattlerBase_maxTpAdd = Game_BattlerBase.prototype.maxTpAdd;
