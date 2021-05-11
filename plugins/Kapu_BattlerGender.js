84244f3279e3aa49b0a5f584bcc7265a66cdcdc9
usagizinrui <usagizinrui@yahoo.co.jp>
2021-05-11 15:51:40 +0900

    [AddNewFeature]Genderプラグインを追加した。動作未確認

---
+++ plugins/Kapu_BattlerGender.js
@@ -0,0 +1,326 @@
+/*:ja
+ * @target MZ 
+ * @plugindesc Game_Actor/Game_Enemyに性別を追加するプラグイン
+ * @author kapuusagi
+ * @url https://github.com/kapuusagi/MZPlugins/tree/master/plugins
+ * 
+ * @command changeActorGender
+ * @text アクターの性別を変更する
+ * @desc アクターの性別を変更する
+ * 
+ * @arg actorId
+ * @text アクターID
+ * @desc アクターID
+ * @type actor
+ * @default 0
+ * 
+ * @arg variableId
+ * @text アクターを表す変数ID
+ * @desc アクターを変数で指定する場合の変数番号。
+ * @type variable
+ * @default 0
+ * 
+ * @arg gender
+ * @text 性別
+ * @desc 変更する性別
+ * @type select
+ * @option 男性
+ * @value 1
+ * @option 女性
+ * @value 2
+ * @option その他
+ * @value 0
+ * @default 1
+ * 
+ * @param textGender
+ * @text 性別を表すテキスト
+ * @desc 性別を表すテキスト。TextManager.genderで提供される。
+ * @type string
+ * @default 性別
+ * 
+ * @param textGenderMale
+ * @text 性別（男性）を表すテキスト
+ * @desc 性別（男性）を表すテキスト。TextManager.genderMaleで提供される。
+ * @type string
+ * @default 男
+ * 
+ * @param textGenderFemale
+ * @text 性別（女性）を表すテキスト
+ * @desc 性別（女性）を表すテキスト。TextManager.genderFemaleで提供される。
+ * @type string
+ * @default 女
+ * 
+ * 
+ * @param textGenderOther
+ * @text 性別（その他）を表すテキスト
+ * @desc 性別（その他）を表すテキスト。TextManager.genderOtherで提供される。
+ * @type string
+ * @default ？
+ * 
+ * @help 
+ * アクター/エネミーに性別を追加します。
+ * スクリプトでの利便性のために用意しています。
+ * $gameParty.isMaleOnly() 
+ *    全員男性の場合にtrue,
+ * $gameParty.isFemaleOnly()
+ *    全員女性の場合にtrue
+ * 
+ * 本プラグイン自体ではあまり意味を持ちません。
+ * 他のプラグインと組み合わせることを想定します。
+ * 例） Kapu_EquipConditionと組み合わせる
+ *     防具のノートタグに <equipCondition:!a.isMale()> とすると、
+ *     男性以外が装備可能になります。
+ * 
+ * 
+ * ■ 使用時の注意
+ * 
+ * ■ プラグイン開発者向け
+ * Game_Actor/Game_Enemy
+ *   gender プロパティ
+ *      性別文字列を返します。ステータス画面での表示など用
+ *   isMale() : boolean
+ *      男性かどうかを判定して返します。
+ *   isFemale() : boolean
+ *      女性かどうかを判定して返します。
+ * 
+ * ============================================
+ * プラグインコマンド
+ * ============================================
+ * プラグインコマンドはありません。
+ * 
+ * ============================================
+ * ノートタグ
+ * ============================================
+ * アクター/エネミー
+ *   <gender:str$>
+ *     性別をstr$に設定する。
+ *     strに指定できるのは
+ *        "1", "male", 性別（男）を表すテキスト
+ *           性別（男）に設定
+ *        "2", "female", 性別（女）を表すテキスト
+ *           性別（女）に設定
+ *        上記以外/未指定
+ *           その他に設定
+ *     プロパティgenderで文字列が取得出来る。
+ * 
+ * ============================================
+ * 変更履歴
+ * ============================================
+ * Version.0.1.0 動作未確認。
+ */
+(() => {
+    const pluginName = "Kapu_BattlerGender";
+    const parameters = PluginManager.parameters(pluginName);
+    const textGender = parameters["textGender"] || "gender";
+    const textGenderMale = parameters["textGenderMale"] || "Male";
+    const textGenderFemale = parameters["textGenderFemale"] || "Female";
+    const textGenderOther =  parameters["textGenderOther"] || "?";
+
+    /**
+     * 性別文字列をパースして性別値を得る。
+     * 
+     * @param {string} str 文字列
+     * @returns {number} 性別を表す定数値
+     */
+    const _parseGender = function(str) {
+        if ((str === "male") || (str === textGenderMale)) {
+            return Game_BattlerBase.GENDER_MALE;
+        } else if ((str === "female") || (str === textGenderFemale)) {
+            return Game_BattlerBase.GENDER_FEMALE;
+        } else {
+            const number = Number(str);
+            if (typeof number !== "undefined") {
+                if (number === Game_BattlerBase.GENDER_MALE) {
+                    return Game_BattlerBase.GENDER_MALE;
+                } else if (number === Game_BattlerBase.GENDER_FEMALE) {
+                    return Game_BattlerBase.GENDER_FEMALE;
+                }
+            }
+        }
+        return Game_BattlerBase.GENDER_OTHER;
+    };
+
+    PluginManager.registerCommand(pluginName, "changeActorGender", args => {
+        let actor = null;
+        const variableId = Number(args.variableId);
+        if (variableid > 0) {
+            const actorId = $gameVariables.value(variableId);
+            actor = $gameActors.actor[actorId];
+        } else {
+            const actorId = Number(args.actorId);
+            actor = $gameActors.actor[actorId];
+        }
+        if (actor !== null) {
+            actor.changeGender(args.gender);
+        }
+    });
+
+    //------------------------------------------------------------------------------
+    // TextManager
+    /**
+     * 性別名を得る。
+     * 
+     * @param {number} genderNo 性別番号
+     * @returns {string} 性別名
+     */
+    TextManager.genderText = function(genderNo) {
+        switch (genderNo) {
+            case Game_BattlerBase.GENDER_MALE:
+                return textGenderMale;
+            case Game_BattlerBase.GENDER_FEMALE:
+                return textGenderFemale;
+            default:
+                return textGenderOther;
+        }
+    };
+    /**
+     * 種族名を得る。
+     * 
+     * @param {number} species 種族番号
+     * @returns {string} 種族名
+     */
+    TextManager.speciesText = function(species) {
+        if ((species > 0) && (species < speciesList.length)) {
+            return speciesList[species];
+        } else {
+            speciesList[0];
+        }
+    };
+
+    // TextManagerプロパティ定義。
+    // ちょっとわかりにくいが、getter(XXXX, id)は
+    // $dataSystem.terms.XXXX[id]
+    Object.defineProperties(TextManager, {
+        /** 性別 */
+        gender: function() { return textGender; },
+        /** 性別（男） */
+        genderMail: TextManager.genderText(Game_BattlerBase.GENDER_MALE),
+        /** 性別（女） */
+        genderFemail: TextManager.genderText(Game_BattlerBase.GENDER_FEMALE),
+        /** 性別（不明） */
+        genderOther: TextManager.genderText(Game_BattlerBase.GENDER_OTHER),
+        /** 種族 */
+        species: function() { return textSpecies; }
+    });
+
+    //------------------------------------------------------------------------------
+    // Game_BattlerBase
+    Game_BattlerBase.GENDER_OTHER = 0;
+    Game_BattlerBase.GENDER_MALE = 1;
+    Game_BattlerBase.GENDER_FEMALE = 2;
+
+    const _Game_BattlerBase_initMembers = Game_BattlerBase.prototype.initMembers;
+    /**
+     * Game_BattlerBaseのパラメータを初期化する。
+     * Game_Enemy, Game_Battlerで独自パラメータを追加する場合、本メソッドをフックする。
+     */
+    Game_BattlerBase.prototype.initMembers = function() {
+        _Game_BattlerBase_initMembers.call(this);
+        this._gender = Game_BattlerBase.GENDER_OTHER;
+    };
+    Object.defineProperty(Game_BattlerBase.prototype, "gender", {
+        get: function() { return TextManager.genderText(this._gender); },
+        configurable: true
+    });
+
+    /**
+     * 男性かどうかを判定する。
+     * 
+     * @returns {boolean} 男性の場合にはtrue, それ以外はfalse
+     */
+    Game_BattlerBase.prototype.isMale = function() {
+        return this._gender === Game_BattlerBase.GENDER_MALE;
+    };
+
+    /**
+     * 女性かどうかを判定する。
+     * 
+     * @returns {boolean} 男性の場合にはtrue, それ以外はfalse.
+     */
+    Game_BattlerBase.prototype.isFemale = function() {
+        return this._gender === Game_BattlerBase.GENDER_FEMALE;
+    };
+    //------------------------------------------------------------------------------
+    // Game_Actor
+    const _Game_Actor_setup = Game_Actor.prototype.setup;
+    /**
+     * このGame_Actorオブジェクトを、actorIdで指定されるアクターのデータで初期化する。
+     * 
+     * @param {Number} actorId アクターID
+     */
+    Game_Actor.prototype.setup = function(actorId) {
+        _Game_Actor_setup.call(this, actorId);
+
+        const dataActor = this.actor();
+        if (dataActor.meta.gender) {
+            this._gender = _parseGender(dataActor.meta.gender);
+        }
+        this.refresh();
+    };
+
+    /**
+     * 性別を変更する。
+     * 
+     * @param {object} gender 性別文字列or性別番号
+     */
+    Game_Actor.prototype.changeGender = function(gender) {
+        this._gender = _parseGender(gender);
+        this.refresh();
+    };
+    //------------------------------------------------------------------------------
+    // Game_Enemy
+    const _Game_Enemy_setup = Game_Enemy.prototype.setup;
+    /**
+     * エネミーをセットアップする。
+     * 
+     * @param {Number} enemyId エネミーID
+     * @param {Number} x X位置
+     * @param {Number} y Y位置
+     */
+    Game_Enemy.prototype.setup = function(enemyId, x, y) {
+        _Game_Enemy_setup.call(this, enemyId, x, y);
+        const dataEnemy = this.enemy();
+        if (dataEnemy.meta.gender) {
+            this._gender = _parseGender(dataEnemy.meta.gender);
+        }
+    };
+
+    //------------------------------------------------------------------------------
+    // Game_Unit
+    /**
+     * 全員男性かどうかを判定する。
+     * 
+     * @returns {boolean} 全員男性の場合にはtrue, それ以外はfalse.
+     */
+    Game_Unit.prototype.isMaleOnly = function() {
+        return this.allMembers().every(member => member.isMale());
+    };
+
+    /**
+     * 全員女性かどうかを判定する。
+     * 
+     * @returns {boolean} 全員女性の場合にはtrue, それ以外はfalse
+     */
+    Game_Unit.prototype.isFemaleOnly = function() {
+        return this.allMembers().every(member => member.isFemale());
+    };
+
+    /**
+     * 男性メンバーを得る。
+     * 
+     * @returns {Array<Game_BattlerBase>} 男性メンバー
+     */
+    Game_Unit.prototype.maleMembers = function() {
+        return this.allMembers().filter(member => member.isMale());
+    };
+    /**
+     * 女性メンバー
+     * 
+     * @returns {Array<Game_BattlerBase>} 女性メンバー
+     */
+    Game_Unit.prototype.femaleMembers = function() {
+        return this.allMembers().filter(member => member.isFemale());
+    };
+
+
+})();
---
+++ plugins/Kapu_CharaMake_BattlerGender.js
@@ -0,0 +1,196 @@
+/*:ja
+ * @target MZ 
+ * @plugindesc キャラメイクで性別選択を追加するプラグイン
+ * @author kapuusagi
+ * @url https://github.com/kapuusagi/MZPlugins/tree/master/plugins
+ * @base Kapu_CharaMake
+ * @orderAfter Kapu_CharaMake
+ * @base Kapu_BattlerGender
+ * @orderAfter Kapu_BattlerGender
+ * 
+ * @command setCharaMakeItemClassesEnabled
+ * @text キャラメイク項目性別を有効にする。
+ * @desc キャラメイク項目性別を有効にする。
+ * @type boolean
+ * @default true
+ * 
+ * @arg isEnabled
+ * @text 有効にする
+ * @type boolean
+ * @default true
+ * 
+ *  
+ * @param isSelectableAtFirst
+ * @text 初期時のみ選択可能とする
+ * @desc 初回作成時のみ選択可能にする場合にはtrueにします。
+ * @type boolean
+ * @default true
+ * 
+ * 
+ * @param textItemNameGender
+ * @text 性別選択項目テキスト
+ * @description キャラクターメイキング項目一覧で、性別に相当する選択項目として表示されるテキスト
+ * @type string
+ * @default 性別
+ * 
+ * @param textItemDescriptionGender
+ * @text 性別名選択項目説明
+ * @description 本項目がセレクト状態になったときに表示する説明テキスト
+ * @type string
+ * @default 性別を変更します。
+ * 
+ * 
+ * @help 
+ * キャラメイクに単純な性別選択を追加するプラグインです。
+ * 
+ * ■ 使用時の注意
+ * 特になし。
+ * 
+ * ============================================
+ * プラグインコマンド
+ * ============================================
+ * プラグインコマンドはありません。
+ * 
+ * ============================================
+ * ノートタグ
+ * ============================================
+ * ありません。
+
+ * ============================================
+ * 変更履歴
+ * ============================================
+ * Version.0.1.0 初版作成。
+ */
+
+/**
+ * 性別選択アイテム
+ */
+ function Game_CharaMakeItem_Gender() {
+    this.initialize(...arguments);
+}
+
+(() => {
+    const pluginName = "Kapu_CharaMake_Gender";
+    const parameters = PluginManager.parameters(pluginName);
+    const textItemNameGender = parameters["textItemNameGender"] || "Class";
+    const textItemDescriptionGender = parameters["textItemDescriptionGender"] || "Select classs.";
+    const isSelectableAtFirst = (typeof parameters["isSelectableAtFirst"] === "undefined")
+            ? true : (parameters["isSelectableAtFirst"] === "true");
+
+    const CHARAMAKEITEM_GENDER = "gender";
+
+    PluginManager.registerCommand(pluginName, "setCharaMakeItemGenderEnabled", args => {
+        const isEnabled = (typeof args.isEnabled === "undefined")
+                ? true : (args.isEnabled === "true");
+        $gameTemp.setCharaMakeItemEnabled(CHARAMAKEITEM_GENDER, isEnabled);
+    });
+
+
+    //------------------------------------------------------------------------------
+    // DataManager
+    const _DataManager_createCharaMakeItems = DataManager.createCharaMakeItems;
+    /**
+     * キャラクターメイキング項目を取得する。
+     * キャラメイク項目を拡張する場合、このメソッドをフックして値を配列に加えて返す。
+     * 
+     * @return {Array<Game_CharaMakeItem>} キャラクターメイキング項目
+     */
+    DataManager.createCharaMakeItems = function() {
+        const items = _DataManager_createCharaMakeItems.call(this);
+        if ($gameTemp.isCharaMakeItemEnabled(CHARAMAKEITEM_GENDER)) {
+            items.push(new Game_CharaMakeItem_Gender());
+        }
+        return items;
+    };
+    //------------------------------------------------------------------------------
+    // Game_CharaMakeItem_Gender
+    Game_CharaMakeItem_Gender.prototype = Object.create(Game_CharaMakeItem.prototype);
+    Game_CharaMakeItem_Gender.prototype.constructor = Game_CharaMakeItem_Gender;
+
+    /**
+     * Game_CharaMakeItem_Genderを初期化する。
+     */
+    Game_CharaMakeItem_Gender.prototype.initialize = function() {
+        Game_CharaMakeItem_Gender.prototype.initialize.call(this, ...arguments);
+    };
+
+    /**
+     * この項目の識別名を取得する。
+     * 
+     * @return {string} キャラクターメイキングの項目名として使用される。
+     */
+    Game_CharaMakeItem_Gender.prototype.name = function() {
+        return textItemNameGender;
+    };
+    /**
+     * この項目の説明を取得する。
+     * 
+     * @return {string} キャラクターメイキングの項目名として使用される。
+     */
+    Game_CharaMakeItem_Gender.prototype.description = function() {
+        return textItemDescriptionGender;
+    };
+
+    /**
+     * アクターに適用可能な項目かどうかを取得する。
+     * 
+     * @param {Game_Actor} actor アクター
+     * @param {boolean} 既存データの変更の場合にはtrue、それ以外はfalse
+     * @returns 適用できる項目の場合にはtrue, それ以外はfalse.
+     */
+    // eslint-disable-next-line no-unused-vars
+    Game_CharaMakeItem_Gender.prototype.canApply = function(actor, isModify) {
+        return !isModify || isSelectableAtFirst;
+    };
+
+    /**
+     * 現在のアクターの情報を反映させる
+     * 
+     * Note: キャラメイク操作で初期値を設定するために呼び出される。
+     * 
+     * @param {object} windowEntry createSelectWindow()で返したウィンドウ
+     * @param {Game_Actor} acotr アクター
+     */
+    // eslint-disable-next-line no-unused-vars
+    Game_CharaMakeItem_Gender.prototype.setCurrent = function(windowEntry, actor) {
+        const selectWindow = windowEntry.selectWindow;
+        const gender = actor.gender;
+        const items = selectWindow.items();
+        let index = 0;
+        while (index < items.length) {
+            if (items[index] === gender) {
+                break;
+            }
+            index++;
+        }
+        if (index < items.length) {
+            selectWindow.select(index);
+        }
+    };
+
+    /**
+     * 編集中の設定を反映させる。
+     * 
+     * @param {object} windowEntry createSelectWindow()で返したウィンドウ
+     * @param {Game_Actor} acotr アクター
+     */
+    // eslint-disable-next-line no-unused-vars
+    Game_CharaMakeItem_Gender.prototype.apply = function(windowEntry, actor) {
+        const selectWindow = windowEntry.selectWindow;
+        const gender = selectWindow.item();
+        actor.changeGender(gender);
+    };
+    /**
+     * アイテム一覧を得る。
+     * 
+     * @return {Array<object>} アイテム一覧
+     */
+    Game_CharaMakeItem_Gender.prototype.items = function() {
+        return [
+            TextManager.genderMale,
+            TextManager.genderFemale,
+            TextManager.genderOther
+        ];
+    };
+
+})();
