dc409952c07a81f88ca0e528207402b1b75dd6df
usagizinrui <usagizinrui@yahoo.co.jp>
2021-06-03 14:29:04 +0900

    [Refactoring]エンカウント率補正の下地をKapu_Base_Mapに入れるようにした。

--- plugins/Kapu_Base_Map.js
+++ plugins/Kapu_Base_Map.js
@@ -40,6 +40,23 @@
  * @min 0.00
  * @max 1.00
  * 
+ * @param encounterProgressValueNormal
+ * @text エンカウント進捗度(通常)
+ * @desc エンカウント進捗度(通常)。1歩毎にこの進捗度を元にした値に補正値がかかったものだけ進む。
+ * @type number
+ * @decimals 2
+ * @default 1.00
+ * @min 0
+ * 
+ * 
+ * @param encounterProgressValueBush
+ * @text エンカウント進捗度(茂み)
+ * @desc エンカウント進捗度(茂み)。1歩毎にこの進捗度を元にした値に補正値がかかったものだけ進む。
+ * @type number
+ * @descimals 2
+ * @default 2.00
+ * @min 0
+ * 
  * @help 
  * 本プラグイン自体にベーシックシステムからの拡張機能は無い。
  * 他のプラグインと競合を避けるために提供されます。
@@ -69,7 +86,8 @@
     const surpriseRateFast = Math.min(surpriseRateLate, Math.max(0, (Number(parameters["surpriseRateFast"]) || 0.03)));
     const preemptiveRateFast = Math.min(1, Math.max(0, (Number(parameters["preemptiveRateFast"]) || 0.05)));
     const preemptiveRateLate = Math.min(preemptiveRateFast, Math.max(0, (Number(parameters["preemptiveRateLate"]) || 0.03)));
-
+    const encounterProgressValueNormal = Number(parameters["encounterProgressValueNormal"]) || 1;
+    const encounterProgressValueBush = Number(parameters["encounterProgressValueBush"]) || 0;
     //------------------------------------------------------------------------------
     // Game_Map
     /**
@@ -88,6 +106,93 @@
     Game_Map.prototype.ratePreemptive = function() {
         return 0;
     };
+
+    /**
+     * ランダムエンカウント進捗度を得る。
+     * 
+     * @param {number} x アクターのx位置
+     * @param {number} y アクターのy位置
+     * @returns {number} 進捗度
+     */
+    Game_Map.prototype.encounterProgressValue = function(x, y) {
+        return this.encounterProgressValueOfPosition(x, y);
+    };
+
+    /**
+     * 位置によるランダムエンカウント進捗度を得る。
+     * 
+     * @param {number} x アクターのx位置
+     * @param {number} y アクターのy位置
+     * @returns {number} 進捗度
+     */
+    Game_Map.prototype.encounterProgressValueOfPosition = function(x, y) {
+        return this.isBush(x, y) ? encounterProgressValueBush : encounterProgressValueNormal;
+    };
+
+    /**
+     * troopIdで指定するエネミーグループに接敵する可能性があるかどうかを得る。
+     * 
+     * @param {number} troopId エネミーグループID
+     * @returns {boolean} 接敵する可能性がある場合にはtrue, それ以外はfalse.
+     */
+    Game_Map.prototype.testEncountCondition = function(troopId) {
+        const dataTroop = $dataTroops[troopId];
+        if (dataTroop) {
+            for (let i = 0; i < dataTroop.members.length; i++) {
+                const member = dataTroop.members[i];
+                if (!member.hidden && this.isEncountToEnemy(member.enemyId)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    };
+
+    /**
+     * enemyIdで指定されるエネミーと遭遇するかどうかを判定する。
+     * 
+     * @param {number} enemyId エネミーID
+     * @returns {boolean} 遭遇する場合にはtrue, 遭遇しない場合にはfalse.
+     */
+    // eslint-disable-next-line no-unused-vars
+    Game_Map.prototype.isEncountToEnemy = function(enemyId) {
+        return true;
+    };
+
+    //------------------------------------------------------------------------------
+    // Game_Player
+    /**
+     * エンカウント進捗度を得る。
+     * Note: 歩行毎に _encounterCount を本カウントだけ減算し、0になったらエンカウントする。
+     * 
+     * @returns {number} エンカウント進捗度
+     * !!!overwrite!!! Game_Player.encounterProgressValue()
+     *     基本エンカウント率をマップから取得出来るように拡張するため、オーバーライトする。
+     */
+    Game_Player.prototype.encounterProgressValue = function() {
+        value = $gameMap.encounterProgressValue(this.x, this.y);
+        if ($gameParty.hasEncounterHalf()) {
+            value *= 0.5;
+        }
+        if (this.isInShip()) {
+            value *= 0.5;
+        }
+        return value;
+    };
+
+    //------------------------------------------------------------------------------
+    // Game_Player
+    const _Game_Player_meetsEncounterConditions = Game_Player.prototype.meetsEncounterConditions;
+    /**
+     * encounterで指定するうグループにエンカウントするかどうかを得る。
+     * 
+     * @param {object} encounter エンカウントデータ
+     * @returns {boolean} エンカウントする場合にはtrue, エンカウントしない場合にはfalse.
+     */
+    Game_Player.prototype.meetsEncounterConditions = function(encounter) {
+        return _Game_Player_meetsEncounterConditions.call(this, encounter)
+            && $gameMap.testEncountCondition(encounter.troopId)
+    };    
     //------------------------------------------------------------------------------
     // Game_Party
     /**
